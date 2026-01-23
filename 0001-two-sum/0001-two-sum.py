0ecf6ca4202676679b17d4e1d721d7107d27ada1
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-01-22 22:09:58 -0800

    Time: 3 ms (51.24%), Space: 20.6 MB (6.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        brute force
+        one is i
+        the other is j
+        find i + j == target
+        O(n^2)
+        """
+        """
+        save nums[i] in hashmap
+        find the target - nums[i]
+        return[i, hashmap[target- nums[i]]]
+        """
         hashmap = {}
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashmap:
                 return [i, hashmap[complement]]
             hashmap[nums[i]] = i
-        return []
+            # can't find it, return []
+        return[]
         
