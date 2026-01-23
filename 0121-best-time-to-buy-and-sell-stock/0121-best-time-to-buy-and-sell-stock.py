610b9a66d2083831c3a1694764e658b4720e7885
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-01-22 22:54:31 -0800

    Time: 36 ms (82.09%), Space: 28.5 MB (29.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        using 2 loops to find the max_profit
+        prices[j] - prices[i] find all possibility
+        TC: O(n^2)
+        SC: O(1)
+        """
+        """
+        find min_price in one loop
+        buy before sell
+        min_price before max_price
+        prices[i] - min_price
+        find the max_profit
+        TC: O(n)
+        SC: O(1)
+        """
+        # define min_price use max number, define max_profit with 0
         min_price = float("inf")
         max_profit = 0
         for i in range(len(prices)):
+            # update min_price
             if prices[i] < min_price:
                 min_price = prices[i]
+            # calculate and update max_profit
             elif prices[i] - min_price > max_profit:
                 max_profit = prices[i] - min_price
         return max_profit
-        """
-        time complexity: O(n)
-        space complexity: O(1)
-        """
-        
