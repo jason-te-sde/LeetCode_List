989afddc15ddb7b60e6d51387658436f45990001
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-01-24 00:05:48 -0800

    Time: 0 ms (100%), Space: 19.4 MB (17.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        first = 1
+        second = 2
+        for i in range(3, n+1):
+            thrid = first + second
+            first = second
+            second = thrid
+        return second
