ff11a994b2dbcc788fc6554bf2783c4bbab110ef
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-01-24 19:36:46 -0800

    Time: 4 ms (50.94%), Space: 19.4 MB (25.36%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,29 +5,23 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        value :[0 - 9]
-        leading zero: 059
-        len of linked list:[1, 100]
-        TC: O(max(m, n))
-        SC: O(1)
-        """
-        dummyHead = ListNode(0)
-        curr = dummyHead
+        DummyHead = ListNode(0)
         carry = 0
-        # 1. Iterate until both lists are exhausted and no carry remains.
+        curr = DummyHead
         while l1 != None or l2 != None or carry != 0:
             l1Val = l1.val if l1 else 0
             l2Val = l2.val if l2 else 0
-            # 2. Calculate the current sum and update the carry for the next position.
-            columnSum = l1Val + l2Val + carry
-            carry = columnSum // 10
-            # 3. Create a new digit node and advance all pointers.
-            newNode = ListNode(columnSum % 10)
+            
+            newVal = (l1Val + l2Val + carry) % 10
+            carry = (l1Val + l2Val + carry) // 10
+            newNode = ListNode(newVal)
+
             curr.next = newNode
-            curr = newNode
+            curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        return dummyHead.next
+        
+        return DummyHead.next
+
 
         
