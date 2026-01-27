176e28eb710f7e731cc3d1cea9f86e16600ba457
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-01-27 01:35:49 -0800

    Time: 0 ms (100%), Space: 19.4 MB (21.05%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        def proc(idx, word):
+            if word[0] not in 'aeiouAEIOU':
+                word = word[1:] + word[0]
+            return word + 'ma' + ('a' * idx)
+            
+        return ' '.join([proc(i, w) for i, w in enumerate(sentence.split(), 1)])
+        
