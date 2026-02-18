bb1aa951ba3256acf696dd9cb1ea10641eb940df
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-02-17 17:16:17 -0800

    Time: 8 ms (90.5%), Space: 49 MB (71.85%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -5,19 +5,18 @@ class Solution {
         }
 
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        List<int[]> merged = new ArrayList<>();
         int[] current = intervals[0];
+        List<int[]> merged = new ArrayList<>();
 
         for(int i = 1; i < intervals.length; i++) {
-            if(intervals[i][0] <= current[1]) {
-                // overlap
+            if(intervals[i][0] <= current[1]){
                 current[1] = Math.max(intervals[i][1], current[1]);
             } else {
                 merged.add(current);
                 current = intervals[i];
             }
         }
-
+        
         merged.add(current);
         return merged.toArray(new int[merged.size()][]);
     }
