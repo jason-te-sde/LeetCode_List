5f3160e6daad6fd068fddb40722dad59f6397270
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-03 11:12:20 -0800

    Time: 7 ms (98.37%), Space: 22.7 MB (38.59%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        codeToGroup = {}
+        ans = collections.defaultdict(list)
         for s in strs:
-            code = self.encode(s)
-            if code not in codeToGroup:
-                codeToGroup[code] = []
-            codeToGroup[code].append(s)
-        
-        res = []
-        for group in codeToGroup.values():
-            res.append(group)
-        
-        return res
-        
-    
-    def encode(self, s: str) -> str:
-        count = [0] * 26
-        for c in s:
-            delta = ord(c) - ord('a')
-            count[delta] += 1
-        return ''.join(map(chr, count))
+            ans[tuple(sorted(s))].append(s)
+        return list(ans.values())
         
