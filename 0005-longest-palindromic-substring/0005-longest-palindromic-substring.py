3e51495b42694dfb64510be716ab7abdbad14136
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-02 16:08:12 -0800

    Time: 231 ms (81.92%), Space: 19.3 MB (86.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,5 +1,9 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        """
+            Time Complexity: O(n^2)
+            Space Complexity: O(1)
+        """
         res = ""
         for i in range(len(s)):
             s1 = self.parlinedrome(s, i, i)
@@ -9,7 +13,7 @@ class Solution:
             res = res if len(res) > len(s2) else s2
 
         return res
-        
+
     def parlinedrome(self, s: str, l: int, r: int) -> str:
         while l >= 0 and r < len(s) and s[l] == s[r]:
             l -= 1
