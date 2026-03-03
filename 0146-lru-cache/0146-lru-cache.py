3198bada88a5b5accd11a3db750111231b842f16
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-02 21:12:21 -0800

    Time: 115 ms (69.77%), Space: 76.9 MB (72.79%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -8,24 +8,17 @@ class LRUCache:
         if key not in self.cache:
             return -1
         
-        self.makeRecently(key)
+        self.cache.move_to_end(key)
         return self.cache[key]
-        
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache[key] = value
-            self.makeRecently(key)
-            return
-        
-        if len(self.cache) >= self.capacity:
-            oldestKey = next(iter(self.cache))
-            self.cache.pop(oldestKey)
+            self.cache.move_to_end(key)
         
         self.cache[key] = value
-    
-    def makeRecently(self, key: int) -> None:
-        value = self.cache.pop(key)
-        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(False)
+
         
 
 
