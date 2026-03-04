3956a1f531b7f611b5c4b10580926bb5c65a10a1
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-03 17:01:57 -0800

    Time: 23 ms (46.96%), Space: 19.3 MB (79.71%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,31 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        left, right = 0, 0
+        have, need = {}, {}
+        valid = 0
+        
+        for c in s1:
+            need[c] = 1 + need.get(c, 0)
+        
+        while right < len(s2):
+            c = s2[right]
+            right += 1
+            # expend window
+            if c in need:
+                have[c] = 1 + have.get(c, 0)
+                if have[c] == need[c]:
+                    valid += 1
+            # shrink window
+            while right - left >= len(s1):
+                if valid == len(need):
+                    return True
+                delete_element = s2[left]
+                left += 1
+                if delete_element in need:
+                    if have[delete_element] == need[delete_element]:
+                        valid -= 1
+                    have[delete_element] -= 1
+        return False
+                    
+                    
+
