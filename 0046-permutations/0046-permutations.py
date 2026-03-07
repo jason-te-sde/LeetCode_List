e4402a9b93f276a745a59a04e70aa58b6c073108
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-07 14:18:25 -0800

    Time: 4 ms (10.29%), Space: 19.5 MB (84.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,25 +1,28 @@
 class Solution:
     def __init__(self):
         self.res = []
-    
     def permute(self, nums: List[int]) -> List[List[int]]:
         track = []
         used = [False] * len(nums)
-
         self.backtrack(nums, track, used)
         return self.res
-    
+
     def backtrack(self, nums, track, used):
+        # end condition
         if len(track) == len(nums):
             self.res.append(track.copy())
             return
         
-        for i in range (len(nums)):
+        for i in range(len(nums)):
+            # illgal choices
             if used[i]:
                 continue
+            # make a decision
             track.append(nums[i])
             used[i] = True
+            # enter into next decision tree
             self.backtrack(nums, track, used)
+            # cancel choices
             track.pop()
             used[i] = False
         
