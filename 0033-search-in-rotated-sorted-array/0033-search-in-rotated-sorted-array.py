187f8d87c6cd123abe56a10cf3af01392c164d43
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-09 20:15:35 -0700

    Time: 0 ms (100%), Space: 19.3 MB (99.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,34 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        """
-            Tag: Binary Search
-            Time Complexity: O(logn)
-            Space Complexity: O(1)
-        """
         left, right = 0, len(nums) - 1
-        # find the index of the pivot element(the smallest element)
         while left <= right:
             mid = left + (right - left) // 2
-            if nums[mid] > nums[-1]:
-                left = mid + 1
-            else:
-                right = mid - 1
-
-        res = self.binarySearch(nums, 0, left - 1, target)
-        if res != -1:
-            return res
-        
-        return self.binarySearch(nums, left, len(nums) - 1, target)
-    
-    def binarySearch(self, nums, left, right, target):
-        while left <= right:
-            mid = left + (right - left) // 2
+            
+            # Case 1: find target
             if nums[mid] == target:
                 return mid
-            elif nums[mid] > target:
-                right = mid - 1
+            
+            # Case 2: subarray on mid's left is sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            
+            # Case 3: subarray on mid's right is sorted
             else:
-                left = mid + 1
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
-
-        
