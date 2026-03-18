2a4747a2b7c755dced1e282f0fc2c1039d66046c
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-03-17 20:18:15 -0700

    Time: 0 ms (100%), Space: 19.3 MB (78.27%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         """
-        TC:O(logN)
-        SC:O(1)
+            Tag: dynamic programming
+            Time Complexity: O(n)
+            Space Complexity: O(n)
         """
-        sqrt5 = 5 ** 0.5
-        phi = (1 + sqrt5) / 2
-        psi = (1 - sqrt5) / 2
-        return int((phi ** (n + 1) - psi ** (n + 1)) / sqrt5)
+        if n <= 2:
+            return n
+        dp = [0] * (n + 1)
+        
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
