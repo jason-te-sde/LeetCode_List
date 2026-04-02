9efc50558de919725d53391afb18ca6b3b865669
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-04-01 21:51:25 -0700

    Time: 46 ms (66.45%), Space: 19.1 MB (73.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
+        memo = [-1] * (n + 1)
+
+        return self.dp(memo, n)
+    
+    def dp(self, memo: list, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        return self.fib(n - 1) + self.fib(n - 2)
+        if memo[n] != -1:
+            return memo[n]
+        
+        memo[n] = self.dp(memo, n - 1) + self.dp(memo, n - 2)
+
+        return memo[n]
         
