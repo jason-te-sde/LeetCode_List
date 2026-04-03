12fa58aff162e2c3c224aac46f58175bdea8f9a1
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-04-03 16:18:09 -0700

    Time: 811 ms (25.72%), Space: 76.7 MB (7.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = [-2] * (amount + 1)
-    
-        def dp(coins: List[int], amount:int) -> int:
-            if amount == 0: return 0 # base case   
-            if amount < 0: return -1 # edge case
-            if memo[amount] != -2: return memo[amount] # check the memo to prevent redundant calculations
-                
-            
-            res = float('inf') # return minimum, so init maximum
-            for coin in coins:# top -> bottom
-                # calcluate the result of the sub-problem
-                subProblem = dp(coins, amount - coin)
-                # skip if the sub-problem has no solution
-                if subProblem == -1:
-                    continue
-                res = min(res, subProblem + 1) # add current coin
+        
+        @cache
+        def dp(rem):
+            if rem == 0: return 0
+            if rem < 0: return -1
 
-            memo[amount] = res if res != float('inf') else -1
-            return memo[amount]
-            
-        return dp(coins, amount)
-            
-
+            res = float('inf')
+            for coin in coins:
+                subProblem = dp(rem - coin)
+                if subProblem != -1:
+                    res = min(res, subProblem + 1)
+            return res if res != float('inf') else -1
+        
+        return dp(amount)
