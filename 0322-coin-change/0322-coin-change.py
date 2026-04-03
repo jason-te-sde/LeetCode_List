948e09659fe261061f5106b8f8c8f67a54210291
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-04-03 16:34:38 -0700

    Time: 499 ms (71.6%), Space: 19.5 MB (96.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,16 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        
-        @cache
-        def dp(rem):
-            if rem == 0: return 0
-            if rem < 0: return -1
+        if amount == 0: # edge case 1
+            return 0
+        if amount < 0: # edge case 2
+            return -1
+        # the maxium number of coins of amount is amount + 1, which means coin denomination is 1, then add 1
+        dp = [amount + 1] * (amount + 1)
+        # init
+        dp[0] = 0
 
-            res = float('inf')
+        for i in range(1, len(dp)):
             for coin in coins:
-                subProblem = dp(rem - coin)
-                if subProblem != -1:
-                    res = min(res, subProblem + 1)
-            return res if res != float('inf') else -1
+                if i - coin < 0:
+                    continue
+                dp[i] = min(dp[i], 1 + dp[i - coin])
         
-        return dp(amount)
+        return dp[amount] if dp[amount] != amount + 1 else -1
+        
+
