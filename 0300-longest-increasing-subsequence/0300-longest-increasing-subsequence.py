a379a131b013c6594c9951753086e7bfa067e7c5
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-05-30 18:08:58 -0700

    Time: 2 ms (96.06%), Space: 19.5 MB (59.95%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,21 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         """
-            tag : dp
-            tc  : O(n^2)
-            sc. : O(n)
+            tg : greedy + bisect
+            tc : O(nlogn)
+            sc : O(n)
         """
-        if not nums:
-            return 0
+        tails = []
 
-        dp = [1] * len(nums)
-        res = 1
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-            
-            res = max(dp[i], res)
+        for num in nums:
+            idx = bisect.bisect_left(tails, num)
 
-        return res
-    
+            if idx == len(tails):
+                tails.append(num)
+            else:
+                tails[idx] = num
+        
+        return len(tails)
