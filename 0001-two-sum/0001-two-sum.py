7ac93e3ad2181dff1ab77cb07fb00c8673a3b304
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-06-05 18:00:05 -0700

    Time: 0 ms (100%), Space: 20.5 MB (18.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
-        return [-1, -1]
+        """
+            tag : two pointers
+            tc. : O(nlogn)
+            sc. : O(n)
+        """
+        pairs = [(value, index) for index, value in enumerate(nums)]
+        pairs.sort()
+        left, right = 0, len(pairs) - 1
+        while left < right:
+            res = pairs[left][0] + pairs[right][0]
+            if res < target:
+                left += 1
+            elif res > target:
+                right -= 1
+            else:
+                return[pairs[left][1], pairs[right][1]]
+        
