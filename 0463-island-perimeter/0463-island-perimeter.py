d1299ec492d96c374591589551904e9dc1cffc39
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-07-07 15:38:35 -0700

    Time: 27 ms (84.42%), Space: 19.6 MB (50.03%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -4,12 +4,12 @@ class Solution:
         n = len(grid[0])
 
         perimeter = 0
-        directions = [[0, 1], [1, 0]]
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
                     perimeter += 4
-                    for x, k in directions:
-                        if 0 <= i + x < m and 0 <= j + k < n and grid[i + x][j + k] == 1:
+                    if 0 <= i + 1 < m and grid[i + 1][j] == 1:
+                            perimeter -= 2
+                    if 0 <= j + 1 < n and grid[i][j + 1] == 1:
                             perimeter -= 2
         return perimeter
