857f4324051e177b554c080f019d233661400892
Jason Te <48173931+jason-te-sde@users.noreply.github.com>
2026-07-14 19:39:45 -0700

    Time: 29 ms (78%), Space: 19.5 MB (86.14%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,11 +1,8 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        """
-            tag : matrix
-            time complexity: O(mn)
-            space complexity: O(1)
+        
 
-        """
+
         m = len(grid)
         n = len(grid[0])
 
@@ -14,10 +11,13 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == 1:
                     perimeter += 4
-                    # right
-                    if 0 <= i + 1 < m and grid[i + 1][j] == 1:
-                            perimeter -= 2
                     # bottom
+                    if 0 <= i + 1 < m and grid[i + 1][j] == 1:
+                        perimeter -= 2
+
+                    #right
                     if 0 <= j + 1 < n and grid[i][j + 1] == 1:
-                            perimeter -= 2
+                        perimeter -= 2
         return perimeter
+
+       
